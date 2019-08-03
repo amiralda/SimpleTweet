912a16150299480297a956d6c738f24fdd7a47f5
amiralda <augustindany@hotmail.com>
2019-08-03 13:23:25 -0400

    First Commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,60 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    private TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        client = TwitterApp.getRestClient(this);
+        populateHomeTimeline();
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeLine(new JsonHttpResponseHandler() {
+            /**
+             * Returns when request succeeds
+             *
+             * @param statusCode http response status line
+             * @param headers    response headers if any
+             * @param response   parsed response if any
+             */
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("TwitterClient", response.toString());
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+            Log.e("TwitterClient", responseString);
+            }
+
+            /**
+             * Returns when request failed
+             *
+             * @param statusCode    http response status line
+             * @param headers       response headers if any
+             * @param throwable     throwable describing the way request failed
+             * @param errorResponse parsed response if any
+             */
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e("TwitterClient", errorResponse.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,10 @@
+package com.codepath.apps.restclienttemplate.models;
+
+public class User {
+    public String name;
+    public long uid;
+    public String screenName;
+    public String profileImageUrl;
+
+
+}
